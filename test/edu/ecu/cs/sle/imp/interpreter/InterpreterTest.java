fcc80b56ba02d5b3758df960cbc74d625ad7c29f
gvsindhura <gangavaramv14@students.ecu.edu>
2015-01-28 00:55:51 -0500

    Assignment 1 - Added Test Cases
    Added Test case Methods for testDivision(), testAddition(),
    testSubtraction() and testUnary()

--- test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
+++ test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
@@ -20,5 +20,26 @@ public class InterpreterTest {
 	public void testMultiply() {
 		IntegerValue iv = interpreter.interpret("1*2");
 		assertEquals("1*2", 2, iv.getIntValue().intValue());
-	}	
+	}
+	
+	@Test
+	public void testDivision() {
+		IntegerValue iv = interpreter.interpret("4/2");
+		assertEquals("4/2", 2, iv.getIntValue().intValue());
+	}
+	@Test
+	public void testAddition() {
+		IntegerValue iv = interpreter.interpret("1+2");
+		assertEquals("1+2", 3, iv.getIntValue().intValue());
+	}
+	@Test
+	public void testSubtraction() {
+		IntegerValue iv = interpreter.interpret("2-1");
+		assertEquals("2-1", 1, iv.getIntValue().intValue());
+	}
+	@Test
+	public void testUnary() {
+		IntegerValue iv = interpreter.interpret("2*-1");
+		assertEquals("2*-1", -2, iv.getIntValue().intValue());
+	}
 }
